eabeeb99a40ea62316cd56e3a48830716936aaef
{bhaskar} <bhaskarvanjarapu207@gmail.com>
2026-07-06 14:23:40 +0530

    fix: use VITE_API_URL for frontend api calls

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   headers: {
     'Content-Type': 'application/json'
   }
